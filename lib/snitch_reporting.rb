258d46cdfba54d7c500029020606ffc41e7398d2
Rocco Nicholls <rocco11nicholls@gmail.com>
2020-01-27 15:19:37 -0700

    Added a top level report method as well

--- lib/snitch_reporting.rb
+++ lib/snitch_reporting.rb
@@ -4,4 +4,7 @@ require "snitch_reporting/rack"
 require "snitch_reporting/version"
 
 module SnitchReporting
+  def self.report(log_level, *args)
+    ::SnitchReporting::SnitchReport.report(log_level, args)
+  end
 end
