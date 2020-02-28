721046bebaa5df61b09a47440b3cf6746968fc67
Rocco Nicholls <rocco11nicholls@gmail.com>
2020-02-28 15:48:32 -0700

    Extracted to helper

--- app/controllers/snitch_reporting/snitch_reports_controller.rb
+++ app/controllers/snitch_reporting/snitch_reports_controller.rb
@@ -2,6 +2,7 @@ require_dependency "snitch_reporting/application_controller"
 
 class ::SnitchReporting::SnitchReportsController < ApplicationController
   include ::SnitchReporting::ParamsHelper
+  include ::SnitchReporting::SnitchReportHelper
   helper ::SnitchReporting::SnitchReportHelper
 
   layout "application"
@@ -132,7 +133,7 @@ class ::SnitchReporting::SnitchReportsController < ApplicationController
       # }
     }
 
-    set_filter_string
+    filter_string_from_params
 
     # @filters = @filter_sets.each_with_object({set_filters: {}}) do |(filter_name, filter_set), filters|
     #   filters[filter_name] = filter_set[:default]
@@ -159,88 +160,10 @@ class ::SnitchReporting::SnitchReportsController < ApplicationController
     # @reports = @filters[:ignored].present? && truthy?(@filters[:ignored]) ? @reports.ignored : @reports.unignored
   end
 
-  def encode_string(token, str)
-    @interpolated_strings ||= []
-
-    @interpolated_strings << str
-    "#{token}(#{@interpolated_strings.length})"
-  end
-
-  def decode_string(token, encoded_str)
-    @interpolated_strings ||= []
-
-    encoded_str.gsub(/(\w+)\((\d+)\)/) do |found|
-      token = Regexp.last_match(1)
-      idx = Regexp.last_match(2)
-
-      if idx.to_i.to_s == idx
-        @interpolated_strings[idx.to_i - 1]
-      else
-        found
-      end
-    end
-  end
-
   def param_safe_value(str)
     return str unless str.include?(" ")
 
     "\"#{str}\""
-  end
-
-  def set_filter_string
-    @filter_string = params[:filter_string] || session[:snitch_filter_string] || "status:unresolved"
-    @unknown_strings = []
-    @filters = {}
-
-    secret_key = loop do
-      token = SecureRandom.hex(10)
-      break token unless @filter_string.include?(token)
-    end
-
-    temp_filter_string = @filter_string.gsub(/\"(.*?)\"/) do |found|
-      encode_string(secret_key, Regexp.last_match(1))
-    end
-
-    search_strings = []
-    tag_strings = []
-    filter_strings = temp_filter_string.split(" ").select do |filter_string|
-      search_strings << filter_string unless filter_string.include?(":")
-      filter_string.include?(":")
-    end
-
-    filter_strings.each do |filter_string|
-      filter, value, * = filter_string.split(":")
-      if value.blank?
-        # Search pure text by itself
-        search_strings << value
-      elsif @filter_sets.keys.include?(filter.to_s.to_sym)
-        value = decode_string(secret_key, value)
-        @filters[filter.to_sym] = value.to_sym
-      elsif filter == "search"
-        search_strings << decode_string(secret_key, value)
-      elsif filter == "tag" || filter == "tags"
-        tag_strings << decode_string(secret_key, value)
-      else
-        @unknown_strings << filter_string
-      end
-    end
-
-    @filters[:search] = "\"#{search_strings.join(' ')}\"" if search_strings.any?
-    @filters[:tag] = tag_strings
-
-    new_filter_strings = @filters.each_with_object([]) do |(filter_string, filter_value), filter_array|
-      next unless filter_value.present?
-      if filter_value.is_a?(Array)
-        filter_value.each do |filter_value_i|
-          filter_array << "#{filter_string}:#{filter_value_i}"
-        end
-      else
-        filter_array << "#{filter_string}:#{filter_value}"
-      end
-    end
-
-    @filter_string = new_filter_strings.join(" ")
-    session[:snitch_filter_string] = @filter_string.dup
   end
   #
   #   def sort_reports
